485bb00c1bf6bd9ca0e4a142a3930c4a6c43cf5d
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-07-11 17:58:54 +0530

    blog website using redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
