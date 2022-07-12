5e988ec846d3e38fc133cdf76cccef24ac876dcf
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-07-12 17:51:58 +0530

    some changes

--- src/action/index.js
+++ src/action/index.js
@@ -1,3 +1,10 @@
-export const fetchPost=()=>{
-    
-}
+import jsonplaceholder from "../api/jsonplaceholder";
+
+export const fetchPost = async () => {
+    const response = await jsonplaceholder.get('/posts')
+    return {
+        type: 'FETCH_POST',
+        payload: response
+    };
+
+};
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPost } from '../action'
 
 export class PostList extends Component {
+    componentDidMount() {
+        this.props.fetchPost();
+    }
     render() {
         return (
             <div>
                 post list
             </div>
-        )    
+        )
     }
 }
 
-export default PostList
+export default connect(null, { fetchPost })(PostList)
